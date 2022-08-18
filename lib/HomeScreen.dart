65b5abb51c0b96a9432d99a28b424fe0a1ef18ae
cfjlu <cfjlu@connect.ust.hk>
2022-08-18 15:36:41 +0800

    homescreen added

---
+++ lib/HomeScreen.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class MainScreen extends StatefulWidget {
+  const MainScreen({Key? key}) : super(key: key);
+
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  @override
+  Widget build(BuildContext context) {
+    double width = MediaQuery.of(context).size.width;
+    double height = MediaQuery.of(context).size.height;
+    return Scaffold(
+      body: Container(
+        height: height,
+        width: width,
+        child: SingleChildScrollView(
+          child: Text('Hello world'),
+        ),
+        
+      ),
+    );
+  }
+}
