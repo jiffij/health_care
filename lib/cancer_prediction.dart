2bd97b523388203504a6a931232105b388e24ed4
Jeff <cfjlu@connect.ust.hk>
2022-12-30 19:29:25 +0800

    cancer_predict file added

---
+++ lib/cancer_prediction.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CancerPredict extends StatefulWidget {
+  const CancerPredict({super.key});
+
+  @override
+  State<CancerPredict> createState() => _CancerPredictState();
+}
+
+class _CancerPredictState extends State<CancerPredict> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
