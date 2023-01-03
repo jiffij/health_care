9e44fcb31d102903ebb8bd4a537bc8a13a8f30a0
cfjlu <cfjlu@connect.ust.hk>
2023-01-04 01:24:00 +0800

    loaded model and image from firebase, model input shape need fix

--- README.md
+++ README.md
@@ -14,3 +14,20 @@ A few resources to get you started if this is your first Flutter project:
 For help getting started with Flutter development, view the
 [online documentation](https://docs.flutter.dev/), which offers tutorials,
 samples, guidance on mobile development, and a full API reference.
+
+## Notes for tflite implementation:
+
+https://github.com/shaqian/flutter_tflite/issues/224
+
+follow the comment of dejduu
+
+1. download zip file from https://github.com/shaqian/flutter_tflite
+
+2. modify the file according to this page https://github.com/shaqian/flutter_tflite/pull/230/files#
+
+3. put the folder into "lib" 
+
+4. add the path to pubspec.yaml as below
+
+    tflite:
+    path: "./lib/flutter_tflite-master"
--- lib/cancer_prediction.dart
+++ lib/cancer_prediction.dart
@@ -1,4 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:simple_login/cancer_model.dart';
+// import 'package:tflite_flutter/tflite_flutter.dart';
+import 'package:simple_login/classifier.dart';
+import 'package:logger/logger.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:image/image.dart' as img;
+import 'dart:io';
+import 'package:simple_login/flutter_tflite-master/lib/tflite.dart';
+
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+// import 'package:firebase_core/firebase_core.dart';
+// import 'package:tflite_flutter_helper/tflite_flutter_helper.dart';
 
 class CancerPredict extends StatefulWidget {
   const CancerPredict({super.key});
@@ -8,8 +22,114 @@ class CancerPredict extends StatefulWidget {
 }
 
 class _CancerPredictState extends State<CancerPredict> {
-  Image _getImg() {
-    return Image.asset('assets/ISIC_0024306.jpg', fit: BoxFit.contain); // TODO
+  // late Classifier _classifier;
+
+  // var logger = Logger();
+
+  // File? _image;
+  // final picker = ImagePicker();
+
+  // Image? _imageWidget;
+
+  // img.Image? fox;
+
+  // Category? category;
+
+  // @override
+  // void initState() {
+  //   super.initState();
+  //   _classifier = cancerClassifier();
+  // }
+
+  // Future _getImage() async {
+  //   final pickedFile = await picker.getImage(source: ImageSource.gallery);
+
+  //   setState(() {
+  //     _image = File(pickedFile!.path);
+  //     _imageWidget = Image.file(_image!);
+
+  //     _predict();
+  //   });
+  // }
+
+  late List _outputs;
+  late File _image;
+  String? _imgUrl;
+
+  @override
+  void initState() {
+    super.initState();
+    _getImg();
+    loadModel().then((value) {
+      setState(() {});
+    });
+  }
+
+  pickImage() async {
+    try {
+      var image = await ImagePicker().pickImage(source: ImageSource.gallery);
+      if (image == null) return null;
+      setState(() {
+        _image = File(image.path);
+      });
+      classifyImage(_image);
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  classifyImage(File image) async {
+    var output = await Tflite.runModelOnImage(
+      path: image.path,
+    );
+    print("predict = " + output.toString());
+    setState(() {
+      _outputs = output!;
+    });
+  }
+
+  loadModel() async {
+    await Tflite.loadModel(
+      model: "assets/My_TFlite_Model.tflite",
+      labels: "assets/label.txt",
+    );
+    print("load model success!");
+  }
+
+  @override
+  void dispose() {
+    Tflite.close();
+    super.dispose();
+  }
+
+  _getImg() async {
+    // return Image.asset('assets/ISIC_0024306.jpg', fit: BoxFit.contain); // TODO
+
+    //current user id
+    final userID = FirebaseAuth.instance.currentUser!.uid;
+
+    //collect the image name
+    DocumentSnapshot variable = await FirebaseFirestore.instance
+        .collection('medical_history')
+        .doc(userID)
+        .get();
+
+    //a list of images names (i need only one)
+    var fileName = variable['upload_img'];
+
+    //select the image url
+    Reference ref = FirebaseStorage.instance
+        .ref()
+        // .child("images/user/profile_images/${_userID}")
+        .child(fileName[0]);
+
+    //get image url from firebase storage
+    var url = await ref.getDownloadURL();
+
+    // put the URL in the state, so that the UI gets rerendered
+    setState(() {
+      _imgUrl = url;
+    });
   }
 
   String _getName() {
@@ -19,6 +139,15 @@ class _CancerPredictState extends State<CancerPredict> {
   String _getCancerLoc() {
     return 'Location from firestore'; //TODO
   }
+
+  // void _predict() async {
+  //   img.Image imageInput = img.decodeImage(_image!.readAsBytesSync())!;
+  //   var pred = _classifier.predict(imageInput);
+
+  //   setState(() {
+  //     this.category = pred;
+  //   });
+  // }
 
   @override
   Widget build(BuildContext context) {
@@ -42,15 +171,13 @@ class _CancerPredictState extends State<CancerPredict> {
                       ),
                     ),
                     Container(
-                      width: width * 0.8,
-                      height: height * 0.4,
-                      child:
-                        _getImg()
-                    ),
+                        width: width * 0.8,
+                        height: height * 0.4,
+                        child: Image.network(_imgUrl.toString())),
                     Padding(
                       padding: EdgeInsets.symmetric(vertical: 8),
                       child: Text(
-                      '''
+                        '''
                       Patient: ${_getName()}
                       Wound location: ${_getCancerLoc()}
                       Result:
@@ -59,6 +186,30 @@ class _CancerPredictState extends State<CancerPredict> {
                         textAlign: TextAlign.left,
                       ),
                     ),
+
+                    Padding(
+                      padding: EdgeInsets.symmetric(vertical: 8),
+                      child: Text(
+                        '''
+                      Result:
+                      ''',
+                        style: TextStyle(fontSize: 18.0),
+                        textAlign: TextAlign.left,
+                      ),
+                    ),
+
+                    ElevatedButton(
+                        onPressed: pickImage, child: Text('Predict')),
+
+                    // Text(
+                    //   category != null ? category!.label : '',
+                    //   style: TextStyle(fontSize: 20, fontWeight: FontWeight.w600),
+                    // ),
+                    // FloatingActionButton(
+                    //   onPressed: _getImage,
+                    //   tooltip: 'Pick Image',
+                    //   child: Icon(Icons.add_a_photo),
+                    // ),
                   ]),
             )));
   }
