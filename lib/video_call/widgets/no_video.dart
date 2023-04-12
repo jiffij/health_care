27e45217d9abc65bb83d0e96e76798cfd536163e
cocojump <tyyipab@connect.ust.hk>
2023-04-12 21:24:10 +0800

    added video_call pack

---
+++ lib/video_call/widgets/no_video.dart
@@ -0,0 +1,22 @@
+
+import 'package:flutter/material.dart';
+import 'dart:math' as math;
+
+import '../theme.dart';
+
+class NoVideoWidget extends StatelessWidget {
+  //
+  const NoVideoWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) => Container(
+        alignment: Alignment.center,
+        child: LayoutBuilder(
+          builder: (ctx, constraints) => Icon(
+            Icons.videocam_off_outlined,
+            color: Colors.black,
+            size: math.min(constraints.maxHeight, constraints.maxWidth) * 0.3,
+          ),
+        ),
+      );
+}
