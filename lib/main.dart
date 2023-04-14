d1f45611c9a72975901b5affb7420c9d766bd7fc
Jeff <cfjlu@connect.ust.hk>
2023-04-14 14:03:13 +0800

    change home

--- lib/main.dart
+++ lib/main.dart
@@ -30,7 +30,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const ConnectPage(),  //new login page
+      home: const loginScreen(),  //new login page
             //const loginScreen(), old login page
     );
   }
